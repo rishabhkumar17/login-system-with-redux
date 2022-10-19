c03e22b0c4e257dfec8aa5bc1c4fef48acca15ea
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-19 23:53:20 +0530

    Login page

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="login">
+      <form className="login__form">
+        <h1>Login Here</h1>
+        <input type="name" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="password" />
+      </form>
+    </div>
+  )
+}
+
+export default Login
