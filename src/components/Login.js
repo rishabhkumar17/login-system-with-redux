90e74e8c609fe75b48877a74a3ad365bcde7673d
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-19 23:59:04 +0530

    states

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,31 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <div className="login">
       <form className="login__form">
         <h1>Login Here</h1>
-        <input type="name" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="password" />
+        <input
+          type="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </form>
     </div>
   )
